a2a8bdb6d4eb706289fc711eb131976fbecbb8d1
Prasanna <prasann@thoughtworks.com>
2014-07-01 17:01:37 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,6 @@
 dredd api.md http://private-97339-themoviedb.apiary-mock.com/
 RESULT=$?
 echo 'About to do a curl...'
-curl http://tws.target.com/productservice/services/item_service/v1/by_itemid?id=1223&alt=json
+curl_result = curl http://tws.target.com/productservice/services/item_service/v1/by_itemid?id=1223&alt=json
+echo $curl_result
 exit $RESULT
