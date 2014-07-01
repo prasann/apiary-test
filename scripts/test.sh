f3e996125e4b8069ecedd002579af886c83c5fb6
Prasanna <prasann@thoughtworks.com>
2014-07-01 16:46:32 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 dredd api.md http://private-97339-themoviedb.apiary-mock.com/
+curl http://tws.target.com/productservice/services/item_service/v1/by_itemid?id=1223&alt=json
 RESULT=$?
 kill -9 $PID
 exit $RESULT
